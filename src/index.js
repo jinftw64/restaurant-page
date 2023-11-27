d11bec49548d399a95c9d7ece15dae7464862c2a
Jin Park <103798287+jinftw64@users.noreply.github.com>
2023-11-27 12:42:40 -0500

    tab switching complete

--- src/index.js
+++ src/index.js
@@ -4,4 +4,53 @@ import contacts from "./contact";
 
 console.log('Test statement');
 
-contacts();
+function createNavBar() {
+  const navBar = document.createElement('nav');
+  const list = document.createElement('ol');
+  const homeLink = document.createElement('li');
+  const menuLink = document.createElement('li');
+  const contactLink = document.createElement('li');
+
+  navBar.appendChild(list);
+  list.appendChild(homeLink);
+  list.appendChild(menuLink);
+  list.appendChild(contactLink);
+
+  homeLink.id = 'id';
+  homeLink.innerText = 'home';
+  menuLink.id = 'menu';
+  menuLink.innerText = 'menu';
+  contactLink.id = 'contact';
+  contactLink.innerText = 'contact';
+
+  homeLink.onclick = () => {
+    clearContent();
+    generatePage();
+  }
+
+  menuLink.onclick = () => {
+    clearContent();
+    menu();
+  }
+
+  contactLink.onclick = () => {
+    clearContent();
+    contacts();
+  }
+
+  return navBar;
+}
+
+function clearContent() {
+  const content = document.querySelector('div#content');
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+}
+
+function renderNavBar() {
+  const nav = document.querySelector('nav');
+  nav.appendChild(createNavBar());
+}
+
+renderNavBar();
